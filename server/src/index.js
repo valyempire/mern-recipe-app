6ed505d0b9cabd473d46659aad4f29f479c1fa36
unknown <valy_empire@yahoo.com>
2024-03-02 17:57:02 +0200

    Created the API for Recipes

--- server/src/index.js
+++ server/src/index.js
@@ -4,6 +4,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 
 import { userRouter } from "./routes/users.js";
+import { recipesRouter } from "./routes/recipes.js";
 
 dotenv.config({
   path: "./src/.env",
@@ -16,6 +17,7 @@ app.use(express.json());
 app.use(cors());
 
 app.use("/auth", userRouter);
+app.use("/recipes", recipesRouter);
 
 mongoose.connect(secretKey);
 
