10e69170709d8dd7c28ebd638c96222a3c9d191a
unknown <valy_empire@yahoo.com>
2024-03-03 15:57:30 +0200

    Refactor the Navbar and removed the vercel.json file

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -3,19 +3,24 @@ import { useCookies } from "react-cookie";
 import { useNavigate } from "react-router-dom";
 
 export const Navbar = () => {
-  const [cookies, setCookies] = useCookies(["access_token"]);
+  const [cookies] = useCookies(["access_token"]);
 
   const navigate = useNavigate();
 
   const logout = () => {
-    setCookies("access_token", "");
+    document.cookie =
+      "access_token=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
     window.localStorage.removeItem("userID");
     navigate("/auth");
   };
   return (
     <div className="navbar">
-      <Link to="/">Home</Link>
-      <Link to="/create-recipe">Create Recipe</Link>
+      {cookies.access_token && (
+        <>
+          <Link to="/">Home</Link>
+          <Link to="/create-recipe">Create Recipe</Link>
+        </>
+      )}
       {!cookies.access_token ? (
         <Link to="/auth">Login/Register</Link>
       ) : (
